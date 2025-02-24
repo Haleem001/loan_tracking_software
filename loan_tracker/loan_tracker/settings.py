f5c083869cd5ef4618694f83ef301ef84a550789
Mahmud Ghali <73344166+Haleem001@users.noreply.github.com>
2025-02-24 12:30:33 +0100

    Update settings.py

--- loan_tracker/loan_tracker/settings.py
+++ loan_tracker/loan_tracker/settings.py
@@ -191,8 +191,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=360),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=360),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
     'ALGORITHM': 'HS256',
